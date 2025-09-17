3add28912f08f882aafa5d1d44c79be39d14b8a8
Increase <increaseakinwole46@gmail.com>
2025-09-17 17:02:33 +0100

    Added Random Color Folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Accordian from "./components/01-accordian";
+import RandomColor from "./components/02-random-color";
 
 function App() {
   return (
     <>
       {/* Accordian component */}
-      <Accordian />
+      {/* <Accordian /> */}
+
+      {/* Random Color component */}
+      <RandomColor />
     </>
   );
 }
